636b9a4b1411aaebbc68935479cfbb40dc11e41a
Denis Zadniprovskiy <dzadnip@gmail.com>
2020-04-06 13:43:51 -0700

    April 1, 2020 Single Number https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3283/

---
+++ SingleNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SingleNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 1, 2, 3, 4 };
+
+            int[] nums2 = new int[] { 1, 3, 1, -1, 3 };
+
+            Console.WriteLine(SingleNumber2(nums));
+        }
+
+        static int SingleNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            int returnNum = nums[nums.Length - 1];
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] != nums[i + 1])
+                {
+                    returnNum = nums[i];
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return returnNum;
+        }
+
+        static int SingleNumber2(int[] nums)
+        {
+            var xor = 0;
+            for (var i = 0; i < nums.Length; i++)
+            {
+                xor ^= nums[i];
+            }
+            return xor;
+        }
+    }
+}
