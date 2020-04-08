fbd2ced29e8e3aa952c09a35f1664b54861b7442
Denis Zadniprovskiy <dzadnip@gmail.com>
2020-04-07 21:31:00 -0700

    April 7, 2020 Counting Elements https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3289/

---
+++ CountElements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //[1,2,3] = 2
+            int[] arr = new int[] {1, 2, 3};
+            Console.WriteLine(CountElements(arr));
+
+            //[1,1,3,3,5,5,7,7] = 0
+
+            //[1,3,2,3,5,0] = 3
+            
+            //[1,1,2,2] = 2
+
+        }
+
+        static int CountElements(int[] arr)
+        {
+            int pairCount = 0;
+            List<int> elements = new List<int>();
+            foreach (int ar in arr)
+            {
+                elements.Add(ar);
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (elements.Contains(arr[i] + 1))
+                {
+                    pairCount++;
+                }
+            }
+            return pairCount;
+        }
+    }
+}
