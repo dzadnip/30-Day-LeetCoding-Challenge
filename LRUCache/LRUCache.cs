5d1348bd57ce26751c7b250569da4839d17a9a79
Denis Zadniprovskiy <dzadnip@gmail.com>
2020-04-24 21:25:28 -0700

    April 24, 2020 LRU Cache https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/531/week-4/3309/

---
+++ LRUCache/LRUCache.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LRUCache
+{
+    class LRUCache
+    {
+        private List<int> _history = new List<int>();
+        private Dictionary<int, int> _cache = new Dictionary<int, int>();
+        private int _capacity = 0;
+
+        public LRUCache(int capacity)
+        {
+            _cache.EnsureCapacity(capacity);
+            _capacity = capacity;
+        }
+
+        public int Get(int key)
+        {
+            if (_cache.ContainsKey(key))
+            {
+                while (_history.Contains(key))
+                {
+                    _history.Remove(key);
+                }
+                _history.Add(key);
+                return _cache[key];
+            }
+            return -1;
+        }
+
+        public void Put(int key, int value)
+        {
+            if (_cache.ContainsKey(key))
+            {
+                _cache[key] = value;
+            }
+            else
+            {
+                if (_cache.Count == _capacity)
+                {
+                    _cache.Remove(_history[0]);
+                    int removeKey = _history[0];
+                    while (_history.Contains(removeKey))
+                    {
+                        _history.Remove(removeKey);
+                    }
+                }
+                _cache.Add(key, value);
+            }
+
+            while (_history.Contains(key))
+            {
+                _history.Remove(key);
+            }
+            _history.Add(key);
+        }
+    }
+}
---
+++ LRUCache/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace LRUCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LRUCache cache = new LRUCache(2);
+            Console.WriteLine("null");
+
+            cache.Put(1, 1);
+            Console.WriteLine("null");
+
+            cache.Put(2, 2);
+            Console.WriteLine("null");
+
+            // 1
+            Console.WriteLine(cache.Get(1));
+
+            cache.Put(3, 3);
+            Console.WriteLine("null");
+
+            // -1
+            Console.WriteLine(cache.Get(2));
+
+            cache.Put(4, 4);
+            Console.WriteLine("null");
+
+            // -1
+            Console.WriteLine(cache.Get(1));
+
+            // 3
+            Console.WriteLine(cache.Get(3));
+
+            // 4
+            Console.WriteLine(cache.Get(4));
+
+
+            Console.WriteLine("");
+
+            cache = new LRUCache(2);
+            Console.WriteLine("null");
+
+            // -1
+            Console.WriteLine(cache.Get(2));
+
+            cache.Put(2, 6);
+            Console.WriteLine("null");
+
+            // -1
+            Console.WriteLine(cache.Get(1));
+
+            cache.Put(1, 5);
+            Console.WriteLine("null");
+
+            cache.Put(1, 2);
+            Console.WriteLine("null");
+
+            // 2
+            Console.WriteLine(cache.Get(1));
+
+            // 6
+            Console.WriteLine(cache.Get(2));
+
+
+
+            Console.WriteLine("");
+
+            cache = new LRUCache(2);
+            Console.WriteLine("null");
+
+            cache.Put(2, 1);
+            Console.WriteLine("null");
+
+            cache.Put(1, 1);
+            Console.WriteLine("null");
+
+            cache.Put(2, 3);
+            Console.WriteLine("null");
+
+            cache.Put(4, 1);
+            Console.WriteLine("null");
+
+            // -1
+            Console.WriteLine(cache.Get(1));
+
+            // 3
+            Console.WriteLine(cache.Get(2));
+        }
+    }
+}
