49509570ed917af73e3552b32ec8150cc9d2ef3c
Denis Zadniprovskiy <dzadnip@gmail.com>
2020-05-02 14:05:28 -0700

    May 2, 2020 Jewels and Stones https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3317/

---
+++ NumJewelsInStones/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace NumJewelsInStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string J = "aA";
+            string S = "aAAbbbb";
+            Console.WriteLine(NumJewelsInStones(J, S));
+        }
+        public static int NumJewelsInStones(string J, string S)
+        {
+            int stonesThatAreJewels = 0;
+            foreach (char s in S)
+            {
+                foreach (char j in J)
+                {
+                    if (s == j)
+                    {
+                        stonesThatAreJewels++;
+                    }
+                }
+            }
+            return stonesThatAreJewels;
+        }
+
+        // Not sure why, but the fastest Runtime Distribution solution used for loops and was 2 times faster
+
+        public static int NumJewelsInStones2(string J, string S)
+        {
+            int count = 0;
+            for (int i = 0; i < J.Length; i++)
+            {
+
+                for (int k = 0; k < S.Length; k++)
+                {
+                    if (J[i] == S[k])
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        // second fastest, using Linq
+        public static int NumJewelsInStones3(string J, string S)
+        {
+            return S.Count(c => J.Contains(c));
+        }
+
+        // Memory Distribution
+
+        public static int NumJewelsInStones11(string J, string S)
+        {
+            int result = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (J.Contains(S[i]))
+                    result++;
+            }
+
+            return result;
+        }
+    }
+}
