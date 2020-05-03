ae8ce5d3195f47cfc028c871f980f64e7325a6cc
Denis Zadniprovskiy <dzadnip@gmail.com>
2020-05-03 00:43:26 -0700

    May 3, 2020 Ransom Note https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3318/

---
+++ CanConstruct/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CanConstruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ransomNote = "ace";
+            string magazine = "abcd";
+            Console.WriteLine(CanConstruct(ransomNote, magazine));
+
+            ransomNote = "ac";
+            magazine = "abcd";
+            Console.WriteLine(CanConstruct(ransomNote, magazine));
+        }
+
+        // My submission 
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            List<char> magazineList = new List<char>();
+            foreach (char c in magazine)
+            {
+                magazineList.Add(c);
+            }
+
+            foreach (char c in ransomNote)
+            {
+                if(magazineList.Contains(c))
+                {
+                    magazineList.Remove(c);
+                } else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Runtime Distribution
+
+        public static bool CanConstruct2(string ransomNote, string magazine)
+        {
+            if (ransomNote.Length > magazine.Length) return false;
+            var letters = new int[('z' - 'a') + 1];
+
+            foreach (char c in magazine) { letters[c - 'a']++; }
+
+            foreach (char c in ransomNote)
+            {
+                if (letters[c - 'a']-- <= 0) return false;
+            }
+
+            return true;
+        }
+
+        public static bool CanConstruct3(string ransomNote, string magazine)
+        {
+            var availableChars = new Dictionary<char, int>();
+            foreach (char c in magazine)
+            {
+                if (!availableChars.ContainsKey(c))
+                    availableChars[c] = 0;
+                availableChars[c]++;
+            }
+
+            foreach (char c in ransomNote)
+            {
+                if (!availableChars.ContainsKey(c) || availableChars[c] < 1)
+                    return false;
+                availableChars[c]--;
+            }
+            return true;
+        }
+
+    }
+}
